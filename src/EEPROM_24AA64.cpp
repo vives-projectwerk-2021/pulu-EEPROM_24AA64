df27527db54c854a67c28d5a8ac50c112c615b0b
Dimi Catrysse <dimicatrysse@outlook.com>
2021-10-17 14:26:48 +0200

    Update library to fix writing pages (32 bytes)
    Add address check
    
    Add debug option

--- src/EEPROM_24AA64.cpp
+++ src/EEPROM_24AA64.cpp
@@ -5,20 +5,121 @@ EEPROM_24AA64::EEPROM_24AA64(PinName sda, PinName scl, uint16_t address):
     this->address = address;
 }
 
-bool EEPROM_24AA64::read(char* data, uint8_t length, uint16_t start_address) {
-    char address[2] = {(char)(start_address>>8), (char)start_address};
-    if( i2c.write(this->address, address, 2, true) ) {
+bool EEPROM_24AA64::read(char* data, uint16_t length, uint16_t start_address) {
+    if(!is_valid_address_range(start_address, length)) {
+        EEPROM_24AA64_DEBUG("read: FAILURE");
         return true;
     }
-    return i2c.read(this->address, data, length);
+    char read_address[2] = {(char)(start_address>>8), (char)start_address};
+    bool ack;
+    EEPROM_24AA64_DEBUG("reading from address: %d (0x%X 0x%X)-- length: %d", start_address, read_address[0], read_address[1], length);
+    ack = i2c.write(address, read_address, 2, true);
+    if(ack) {
+        EEPROM_24AA64_DEBUG("read: FAILURE while writing address");
+        return true;
+    }
+    ack = i2c.read(address, data, length);
+    if(ack) {
+        EEPROM_24AA64_DEBUG("read: FAILURE while reading");
+        return true;
+    }
+    wait_till_ready();
+    EEPROM_24AA64_DEBUG("read: DONE");
+    return false;
 }
 
-bool EEPROM_24AA64::write(char* data, uint8_t length, uint16_t start_address) {
-    char buffer[length+2] = {(char)(start_address>>8), (char)start_address};
-    memcpy(buffer+2, data, length);
-    if( i2c.write(address, buffer, length+2) ) {
+bool EEPROM_24AA64::write(char* data, uint16_t length, uint16_t start_address) {
+    if(!is_valid_address_range(start_address, length)) {
+        EEPROM_24AA64_DEBUG("write: FAILURE");
         return true;
     }
-    wait_us(5000); //wait till write cycle ends (5ms)
+    bool ack;
+    char* pointer = data;
+    uint16_t write_address = start_address;
+    uint8_t first_block_size = PAGE_SIZE-(start_address%PAGE_SIZE);
+    if(first_block_size>length) { first_block_size = length;}
+    uint8_t blocks = (length-first_block_size)/PAGE_SIZE;
+    uint8_t last_block_size = (length-first_block_size) - (blocks*PAGE_SIZE);
+    EEPROM_24AA64_DEBUG("writing to address: %d -- total length: %d -- first block: %d bytes - blocks: %d x %d bytes - last block: %d bytes", start_address, length, first_block_size, blocks, PAGE_SIZE, last_block_size);
+    // first block
+    if(first_block_size) {
+        ack = write_block(pointer, first_block_size, write_address);
+        pointer += first_block_size;
+        write_address += first_block_size;
+        if(ack) {
+            EEPROM_24AA64_DEBUG("write: FAILURE (first block)");
+            return true;
+        }
+        EEPROM_24AA64_DEBUG("write: SUCCESS (first block)");
+    }
+    // middle blocks
+    if(blocks) {
+        for(uint8_t block = 0; block<blocks; block++) {
+            ack = write_block(pointer, PAGE_SIZE, write_address);
+            pointer += PAGE_SIZE;
+            write_address += PAGE_SIZE;
+            if(ack) {
+                EEPROM_24AA64_DEBUG("write: FAILURE (block %d)", block);
+                return true;
+            }
+            EEPROM_24AA64_DEBUG("write: SUCCESS (block %d)", block);
+        }
+    }
+    // last block
+    if(last_block_size) {
+        ack = write_block(pointer, last_block_size, write_address);
+        if(ack) {
+            EEPROM_24AA64_DEBUG("write: FAILURE (last block)");
+            return true;
+        }
+        EEPROM_24AA64_DEBUG("write: SUCCESS (last block)");
+    }
+    EEPROM_24AA64_DEBUG("write: DONE");
     return false;
+}
+
+bool EEPROM_24AA64::write_block(char* data, uint8_t length, uint16_t write_address) {
+    bool ack;
+    char* buffer = (char*)malloc(length+2);
+    buffer[0] = (char)(write_address>>8);
+    buffer[1] = (char)write_address;
+    for(uint8_t i = 0; i<length; i++) {
+        buffer[i+2] = data[i];
+    }
+    ack = i2c.write(address, buffer, length+2);
+    if(ack) {
+        free(buffer);
+        return true;
+    }
+    wait_till_ready();
+    free(buffer);
+    return false;
+}
+
+void EEPROM_24AA64::wait_till_ready() {
+    bool ack;
+    char cmd[2] = {0,0};
+    EEPROM_24AA64_DEBUG("waiting till ready");
+    do {
+        ack = i2c.write(address, cmd, 2);
+        wait_us(500);
+    } while(ack != 0);
+    EEPROM_24AA64_DEBUG("ready");
+}
+
+bool EEPROM_24AA64::is_valid_address_range(uint16_t address, uint16_t length) {
+    if(address>MAX_ADDRESS) {
+        EEPROM_24AA64_DEBUG("ADDRESS FAILURE: start_address (%d) > %d", address, MAX_ADDRESS);
+        return false;
+    }
+    uint16_t end_address = address + length;
+    if(end_address>MAX_ADDRESS) {
+        EEPROM_24AA64_DEBUG("ADDRESS FAILURE: end_address (%d) > %d", end_address, MAX_ADDRESS);
+        return false;
+    }
+    if(end_address<address) {
+        EEPROM_24AA64_DEBUG("ADDRESS FAILURE: end_address (%d) < start_address (%d)", end_address, address);
+        return false;
+    }
+    return true;
 }
--- src/EEPROM_24AA64.h
+++ src/EEPROM_24AA64.h
@@ -2,14 +2,26 @@
 
 #include "mbed.h"
 
+#define EEPROM_24AA64_DEBUG(x, ...)   //set as comment to enable debugging
+#ifndef EEPROM_24AA64_DEBUG
+#define EEPROM_24AA64_DEBUG(x, ...) printf("[EEPROM_24AA64]\t" x "\r\n", ##__VA_ARGS__)
+#endif
+
 class EEPROM_24AA64 {
     public:
         EEPROM_24AA64(PinName sda, PinName scl, uint16_t address);
     
-        bool read(char* data, uint8_t length, uint16_t start_address = 0); // success = 0
-        bool write(char* data, uint8_t length, uint16_t start_address);   // success = 0
+        bool read(char* data, uint16_t length, uint16_t start_address = 0); // success = 0
+        bool write(char* data, uint16_t length, uint16_t start_address);   // success = 0
+    
+    private:
+        bool write_block(char* data, uint8_t length, uint16_t write_address);
+        void wait_till_ready();
+        bool is_valid_address_range(uint16_t address, uint16_t length);
     
     private:
         I2C i2c;
         uint16_t address;
+        const uint16_t MAX_ADDRESS = 0x1FFF;
+        const uint8_t PAGE_SIZE = 32;
 };
